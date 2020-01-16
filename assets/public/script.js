593d3293ab219f0c17ffa12a64e43b324d01abfb
Tomás Nicolorich <tomasnicolorich@gmail.com>
2020-01-16 17:32:21 +0100

    card structure

--- assets/public/script.js
+++ assets/public/script.js
@@ -1,14 +1,32 @@
-var text = document.getElementById("text");
+// Makes cards
+var main = document.getElementById("main");
+
+var fontFamilies = ["Georgia", "Palatino Linotype", "Times New Roman"];
+
+for (var i = 0; i < fontFamilies.length; i++) {
+  var e = document.createElement("div");
+  e.setAttribute("class", "card");
+  e.innerHTML = `<div class="title"><h3>${fontFamilies[i]}</h3></div><div class="author"><h5>Author</h5></div><div class="example"> <h1 class="example-text">The quick brown fox.</h1></div>`;
+  main.appendChild(e);
+}
+
+// Change text with the sample text
+var input = document.getElementById("sample");
+var text = document.querySelectorAll(".example-text");
+
+input.addEventListener("input", function() {
+  text.forEach(function(e) {
+    if (input.value === "") {
+      e.innerHTML = "The quick brown fox.";
+    } else {
+      e.innerHTML = input.value;
+    }
+  });
+});
+
+// Link pixel size number with slider
 var pixel = document.getElementById("pixel");
 var pixelSlide = document.getElementById("pixelSlide");
-var input = document.getElementById("sample");
-input.addEventListener("input", function() {
-  if (input.value == "") {
-    text.innerHTML = "Hello World!";
-  } else {
-    text.innerHTML = input.value;
-  }
-});
 
 pixelSlide.addEventListener("input", function() {
   pixel.value = pixelSlide.value;
