27fef57837a822e7f1553cde8ada0ed58fd687a8
Tomás Nicolorich <tomasnicolorich@gmail.com>
2020-01-16 15:54:51 +0100

    Nav structure AND FontAwesome

--- assets/public/script.js
+++ assets/public/script.js
@@ -1,9 +1,19 @@
 var text = document.getElementById("text");
-var input = document.getElementById("search");
+var pixel = document.getElementById("pixel");
+var pixelSlide = document.getElementById("pixelSlide");
+var input = document.getElementById("sample");
 input.addEventListener("input", function() {
   if (input.value == "") {
     text.innerHTML = "Hello World!";
   } else {
     text.innerHTML = input.value;
   }
+});
+
+pixelSlide.addEventListener("input", function() {
+  pixel.value = pixelSlide.value;
+});
+
+pixel.addEventListener("input", function() {
+  pixelSlide.value = pixel.value;
 });
